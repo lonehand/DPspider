fffdcaade262e7af43fed28011297fca5686b36c
johnny leaf <fxtafreeman@163.com>
2018-08-13 18:37:20 +0800

    18:37

--- Spider_edp/dpspider.py
+++ Spider_edp/dpspider.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
 
+import time
+
 import requests
-import random
 from bs4 import BeautifulSoup
-import json
-import datetime
+from selenium import webdriver
+import selenium.webdriver.support.ui as ui
+
+
+# 账号与密码
+Account = ["TFmeirui", "cdjianli","bjykyl"]
+Password = ["cdmeirui123","cdjianli123","ykyl180225"]
 
 # 伪装浏览器头
 header = {
@@ -13,15 +19,22 @@ header = {
     "Accept-Language": "zh-CN,zh;q=0.9",
     "Cache-Control": "max-age=0",
     "Connection": "keep-alive",
-    "Cookie": r"_lxsdk_cuid=1651dbc8f58c8-036d4b7b2984bd-9393265-1fa400-1651dbc8f59c8; _lxsdk=1651dbc8f58c8-036d4b7b2984bd-9393265-1fa400-1651dbc8f59c8; _hc.v=\cc1d08d6-9c75-4b66-bacd-ba60e886bcf4.1533802221\; _lxsdk_s=165217310bc-f80-f7e-808%7C%7C8",
     "Host": "e.dianping.com",
     "Upgrade-Insecure-Requests": "1",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36"
 }
 
+# 登陆账号
+post_data = {
+    "login" : "MeiruiTF",
+    "loginTyoe" : "account",
+    "password" : "cdmeirui123",
+}
 
 # 目标网址
-target = "http://e.dianping.com"
+target1 = "http://e.dianping.com/slogin"
+target2 = "https://e.dianping.com/shopaccount/login/setedper?targetUrl=https://e.dianping.com/shopportal/newindex"
+target3 = "https://e.dianping.com/shopportal/newindex"
 
 # 备用字典
 DataInfo = {'status_code':'','headers':'' ,'html_code':''}
@@ -46,15 +59,16 @@ def login(self,posturl,postdata):
 # 请求商家后台，并提供相应返回值
 def get_account(target, header): #请求商家后台
     try:
-        res = requests.get(target, headers=header)
-        if res.status_code == 200:
-            return res.text
-        else:
-            return ErrorInfo+res.status_code
+        ChromeBrowser = webdriver.Chrome()
+        ChromeBrowser.get(target)
+        wait = ui.WebDriverWait
+        ChromeBrowser.find_element_by_xpath('//*[@id="login"]//div[2]').send_keys(Account[0])
+        ChromeBrowser.find_element_by_xpath('//*[@id="password"]//div[2]').send_keys(Password[0])
+        ChromeBrowser.find_element_by_xpath('//*[@id="login-form"]/button').submit()
     except Exception as errorinfo:
         return errorinfo
 
 # 主函数
 if __name__ == "__main__": #主函数
-    Webdata = get_account(target, header)
+    Webdata = get_account(target1, header)
     print(Webdata)
