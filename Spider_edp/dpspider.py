8cc825e38ecddb480b7bd01d62260cadd090e80f
johnny leaf <fxtafreeman@163.com>
2018-08-17 18:55:02 +0800

    18:54

--- Spider_edp/dpspider.py
+++ Spider_edp/dpspider.py
@@ -89,6 +89,7 @@ if __name__ == "__main__":  # 主函数
 
     IndexCookies = Get_CookeandSession(LoginUrl)
     Response = Get_Data(TargetUrl, IndexCookies)
+    print(Response)
     a = DataOptimization(Response)
 
 
